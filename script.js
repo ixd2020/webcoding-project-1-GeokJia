985f32bc3e767b8e416eb54637380df50f618dd7
Glitch (oscar-claude-monet) <none>
2020-12-13 11:48:23 +0000

    🐪👆 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -34,8 +42,6 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-
-
 anime
   .timeline({ loop: false })
   .add({
@@ -49,7 +55,7 @@ anime
     targets: ".ml5 .line",
     duration: 600,
     easing: "easeOutExpo",
-    translateY: (el, i) => -1.12 + 1.12 * 2 * i + "em"
+    translateY: (el, i) => -1.17 + 1.17 * 2 * i + "em"
   })
   .add({
     targets: ".ml5 .ampersand",
@@ -79,14 +85,6 @@ anime
     targets: ".ml5"
   });
 
-function myFunction() {
-  var x = document.getElementById("topnav");
-  if (x.className === "navi") {
-    x.className += " responsive";
-  } else {
-    x.className = "navi";
-  }
-}
 
 function function1() {
   document.getElementById("hay").style.width = "120%";
@@ -138,3 +136,106 @@ function function15() {
 function function16() {
   document.getElementById("others").style.width = "100%";
 }
+
+function h1() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstacks at Giverny, sunset. (1888–1889) <br>Medium: Oil on canvas";
+}
+
+function h2() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystacks. (1890) <br> Medium: Oil on canvas.<br>This painting was sold for $110.7 million on Sotheby's May 14, 2019.";
+}
+
+function h3() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstacks, Snow Effect (1891)";
+}
+
+function h4() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystack, Morning Snow Effect (1891) Medium: Oil on canas.";
+}
+
+function h5() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstack. (Sunset) (1890-1891) <br>Medium: Oil on canvas";
+}
+
+function h6() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Wheatstacks (End of Summer). (1890-1891) <br>Medium: Oil on canvas";
+}
+
+function h7() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Monet – Wildenstein 1996, 1272";
+}
+
+function h8() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstacks, Snow Effect (1890-1891)";
+}
+
+function h9() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystacks at sunset, frosty weather";
+}
+
+function h10() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstack in Sunshine. (1891) <br>Medium: Oil on canvas";
+}
+
+function h11() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstack at Giverny. (1888–1889) <br>Medium: Oil on canvas";
+}
+
+function h12() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Wheatstacks. (1890-1891) <br>Medium: Oil on canvas";
+}
+function h13() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystacks, (Midday). (1890–1891) <br>Medium: Oil on canvas";
+}
+function h14() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystacks (Effect of Snow and Sun). <br>Medium: Oil on canvas";
+}
+function h15() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Wheatstack (Sun in the Mist). (1891) <br>Medium: Oil on canvas";
+}
+
+function h16() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Haystacks, (Midday). (1890–91) <br>Medium: Oil on canvas";
+}
+
+function h17() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Grainstacks, White Frost Effect. (1889) <br>Medium: Oil on canvas";
+}
+function h18() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Stacks, End of Summer. (1891) <br>Medium: Oil on canvas";
+}
+
+function h19() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Wheatstacks (Sunset, Snow Effect). (1890–91) <br>Medium: Oil on canvas";
+}
+
+function h20() {
+  document.getElementById("haystack").innerHTML =
+    "Title: Wheatstack (Snow Effect, Overcast day). (1890–91) <br>Medium: Oil on canvas";
+}
+
+$(document).ready(function(){
+  $("button").hover(function(){
+    $("#div1").toggle(200);
+
+  });
+});
